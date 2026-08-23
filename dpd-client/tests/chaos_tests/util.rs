9df3b85cd52957cf62fca451fbddf938736dbf0b
Cory Zimmerman <coryzimmerman93@gmail.com>
2026-08-23 16:58:22 +0000

    Add repro tests for address isolation issue
    - Tagged port_settings_apply can delete addresses from another tag.
    - Tagged port_settings_get returns addresses belonging to other tags.
    - Tagged port_settings_clear can delete addresses from another tag.

--- dpd-client/tests/chaos_tests/util.rs
+++ dpd-client/tests/chaos_tests/util.rs
@@ -4,9 +4,19 @@
 //
 // Copyright 2025 Oxide Computer Company
 
-use dpd_client::Client;
-use dpd_client::types::{Ipv4Entry, Ipv6Entry};
+use std::collections::HashSet;
+use std::net::IpAddr;
+use std::net::Ipv4Addr;
+use std::net::Ipv6Addr;
+
 use futures::TryStreamExt;
+use rand::Rng;
+use rand::SeedableRng;
+use rand::rngs::StdRng;
+
+use dpd_client::Client;
+use dpd_client::types::Ipv4Entry;
+use dpd_client::types::Ipv6Entry;
 
 pub(crate) async fn link_list_ipv4(
     client: &Client,
@@ -36,4 +46,65 @@ pub(crate) async fn link_list_ipv6(
         )
         .try_collect::<Vec<Ipv6Entry>>()
         .await
+}
+
+/// A random IP address generator.
+pub struct IpRng {
+    rng: StdRng,
+    claimed: HashSet<IpAddr>,
+}
+
+impl IpRng {
+    /// Creates a new ip address generator from the given seed.
+    pub fn new(seed: u64) -> Self {
+        Self { rng: StdRng::seed_from_u64(seed), claimed: HashSet::default() }
+    }
+
+    /// Returns a random IPv4 address that this instance
+    /// has never created before.
+    pub fn unique_ipv4(&mut self) -> Ipv4Addr {
+        Self::roll_unique(&mut self.claimed, || {
+            Ipv4Addr::from_bits(self.rng.random())
+        })
+    }
+
+    /// Returns a random IPv6 address that this instance
+    /// has never created before.
+    pub fn unique_ipv6(&mut self) -> Ipv6Addr {
+        Self::roll_unique(&mut self.claimed, || {
+            Ipv6Addr::from_bits(self.rng.random())
+        })
+    }
+
+    fn roll_unique<T>(
+        tracker: &mut HashSet<IpAddr>,
+        mut random_addr: impl FnMut() -> T,
+    ) -> T
+    where
+        T: Into<IpAddr> + Copy,
+    {
+        loop {
+            // Executes infinitely if we've already generated the entire
+            // IPv4 or IPv6 address space, in which case the offending test
+            // has earned a more bespoke solution :)
+            let addr = random_addr();
+            if tracker.insert(addr.into()) {
+                return addr;
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod util_tests {
+    use crate::chaos_tests::util::IpRng;
+
+    /// Basic check on unique ipv6 address generation.
+    #[test]
+    fn unique_v6() {
+        let mut rng = IpRng::new(7);
+        let one = rng.unique_ipv6();
+        let two = rng.unique_ipv6();
+        assert_ne!(one, two);
+    }
 }
